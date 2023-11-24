cb48f9eeef70e308f155ea69d65a710e2716f781
Dakota Ramey <dakotaramey6@gmail.com>
2023-11-24 13:02:24 -0500

    Fixed file removing bug also added a title and format of the todos

---
+++ README.md
@@ -0,0 +1,8 @@
+# TO DO LIST v1.0
+
+## How to download/ use?
+
+- Unzip downloaded files
+- Make sure main.py is in a folder of anykind
+- run the GUI
+- Make your todo list
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from cProfile import label
 from tkinter import *
 
 window = Tk()
+title_of_todo = Label(window, text='My to do list',
+                      width="250", font='Cursive')
+title_of_todo['background'] = '#856ff8'
+title_of_todo.pack()
 
-user_input = Entry(window, text='o ')
+user_input = Entry(window)
+list_of_todo = []
 
 user_input.pack(pady=4, padx=4)
 items_in_list = Listbox(window, height=22, width=30)
@@ -17,37 +21,39 @@ def init():
         items = get_values.readlines()
         for i in items:
             items_in_list.insert(END, i)
+            list_of_todo.append(i)
 
 
 def save_to_file(what_to_save):
+    list_of_todo.append(what_to_save)
     with open('task.txt', 'a') as save_file:
-        save_file.write(f'{what_to_save}\n')
+        save_file.write(f'o {what_to_save}\n')
 
 
 def add_to_do(event):
     to_do_string = user_input.get()
     if len(to_do_string) > 0:
-        items_in_list.insert(END, to_do_string)
+        items_in_list.insert(END, 'o '+to_do_string)
         save_to_file(to_do_string)
         user_input.delete(0, END)
 
 
 def remove_to_do(event):
+    string_of_slected_item = items_in_list.get(ACTIVE)
     items_in_list.delete(ACTIVE)
-    with open('task.txt', 'r+') as file_remove_output:
-        lines = file_remove_output.readlines()
 
-        file_remove_output.truncate()
-        for line in lines:
-            if items_in_list.get(ACTIVE) == line:
-                lines.remove(line)
-            file_remove_output.write(line)
+    for item_To_delete in list_of_todo:
+        if item_To_delete == string_of_slected_item:
+            list_of_todo.remove(item_To_delete)
+    with open('task.txt', 'w') as file_remove_output:
+        for i in list_of_todo:
+            file_remove_output.writelines(i)
 
 
 init()
 remove_btn.bind('<Button-1>', remove_to_do)
 window.bind('<Return>', add_to_do)
 window.title('To Do List')
-window.geometry('250x440+10+10')
+window.geometry('250x460+10+10')
 window['background'] = '#856ff8'
 window.mainloop()
